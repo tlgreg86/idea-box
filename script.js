c616462f43e0f017091b9439d86df8e53b158989
Jack Bevis <jack.bevis@gmail.com>
2017-02-14 17:21:52 -0700

    Add retrieve ideas on page load, display cards correclty, persistence in DOM on refresh, cleaned up code base

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//Retrieve ideas on page load
+$(document).ready(retrieveIdeas);
+
+
 //constructor
 function Idea(id, title, body, quality) {
-  this.id = Date.now();
+  this.id = id;
   this.title = title;
   this.body = body;
   this.quality = quality || 'swill';
 }
 
 
-//keyup to enable button...keyup to disable
+//button disabled
 $('#saveButton').prop('disabled', true);
 
+//input field keyup
 $('input[type=text]').on('keyup', function(){
   var title = $('#titleInput').val();
   var body = $('#bodyInput').val();
@@ -21,86 +26,19 @@ $('input[type=text]').on('keyup', function(){
 });
 
 
-//click event on save button...generates new idea card...prepends new card to idea-box section
-//counter function to assign unique id to card.
-//clear input fields on save click.
-
-///////////working card function///////////////
-$("#saveButton").on("click", function(event){
-  // var id = Date.now();
-  var newIdea = new Idea (id, title, body);
-  var storedObj = getIdea (id);
+//Save button click event
+$("#saveButton").on("click", function(){
   var title = $('#titleInput').val();
   var body = $('#bodyInput').val();
-  var id = getID(this);
-  console.log(id)
-  event.preventDefault();
-  // storeIdea(id, storedObj);
+  var id = Date.now();
+  var newIdea = new Idea (id, title, body);
+  Idea();
+  createCard(newIdea);
   storeIdea(id, newIdea);
-  getIdea(id);
-  createCard();
   clearInputs();
 
 })
 
-
-//
-// function createCard($title, $body) {
-//   $('#ideaBox').prepend(
-//     `<article class="card" id="card">
-//       <h2>${$title}</h2>
-//       <img class="card-delete" src="images/delete.svg" alt="delete">
-//       <p>${$body}</p>
-//       <img class="upvote-button" src="images/upvote.svg" alt="upvote button">
-//       <img class="downvote-button" src="images/downvote.svg" alt="downvote button">
-//       <p>quality:<span>swill</span></p>
-//       <hr>
-//     </article>`
-//   )};
-
-//constructor function
-
-
-//takes input values and assigns to new constructor
-// function recordInput (id) {
-//   var title = $('.title-input').val();
-//   var body = $('.body-input').val();
-//   var newIdea = new Idea (id, title, body);
-//   var storedObj = getIdea (id);
-//   var id = getID(this)
-//   storeIdea (id, storedObj)
-// }
-
-function storeIdea (id, card) {
-  localStorage.setItem(id, JSON.stringify(card));
-}
-
-function getIdea (id, card) {
-  return JSON.parse(localStorage.getItem(id))
-}
-
-function getID (selector) {
-  return $(selector).closest('.card').attr('id')
-}
-
-
-//create new card to display in DOM
-function createCard () {
-  // var id = Date.now();
-  // var title = $('.title-input').val();
-  // var body = $('.body-input').val();
-  // var quality = 'swill'
-  $('#ideaBox').prepend(
-      `<article class="card" id="${this.id}">
-        <h2>${this.title}</h2>
-        <img class="card-delete" src="images/delete.svg" alt="delete">
-        <p>${this.body}</p>
-        <img class="upvote-button" src="images/upvote.svg" alt="upvote button">
-        <img class="downvote-button" src="images/downvote.svg" alt="downvote button">
-        <p>quality:<span>${this.quality}</span></p>
-        <hr>
-      </article>`
-)}
 
 //clear inputs
 function clearInputs () {
@@ -108,6 +46,38 @@ function clearInputs () {
   $('.body-input').val("");
 }
 
+
+//store idea to localStorage
+function storeIdea (id, card) {
+  localStorage.setItem(id, JSON.stringify(card));
+}
+
+
+//retrieve stored ideas
+function retrieveIdeas () {
+  for (var key in localStorage) {
+    var parsedIdea = JSON.parse(localStorage[key]);
+    createCard(parsedIdea);
+  }
+}
+
+
+//create new card to display in DOM
+function createCard (idea) {
+  $('#ideaBox').prepend(
+      `<article class="card" id="${idea.id}">
+        <h2>${idea.title}</h2>
+        <img class="card-delete" src="images/delete.svg" alt="delete">
+        <p>${idea.body}</p>
+        <img class="upvote-button" src="images/upvote.svg" alt="upvote button">
+        <img class="downvote-button" src="images/downvote.svg" alt="downvote button">
+        <p>quality:<span>${idea.quality}</span></p>
+        <hr>
+      </article>`
+)}
+
+
+//delete button on card
 //delete card from DOM and storage
 // $(".idea-box").on("click", ".card-delete", function() {
 //   var id = $(this).parent().attr("id");
@@ -118,21 +88,6 @@ function clearInputs () {
 
 
 
-// newIdea.prototype.createCard = function(){
-//   return `<article class="card" id="card">
-//     <h2>${title}</h2>
-//     <img class="card-delete" src="images/delete.svg" alt="delete">
-//     <p>${body}</p>
-//     <img class="upvote-button" src="images/upvote.svg" alt="upvote button">
-//     <img class="downvote-button" src="images/downvote.svg" alt="downvote button">
-//     <p>quality:<span>swill</span></p>
-//     <hr>
-//   </article>`
-// }
-
-
-//delete button on card
-
 
 //vote up vote down buttons
 
@@ -140,4 +95,4 @@ function clearInputs () {
 //filter ideas via search bar
 
 
-//Title and body text need to be editable on the card, changes saved via enter/return keystrok or clicking outside of field. Changes persist even on page reload.
+//Title and body text need to be editable on the card, changes saved via enter/return keystroke or clicking outside of field. Changes persist even on page reload.
